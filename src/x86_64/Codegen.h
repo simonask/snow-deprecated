253f65b9980c1d29aead61efe46b7d139f8290d0
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-12-17 15:16:06 +0100

    codegen: work-in-progress

---
+++ src/x86_64/Codegen.h
@@ -0,0 +1,17 @@
+#ifndef CODEGEN_H_1VNJIVK0
+#define CODEGEN_H_1VNJIVK0
+
+#include <Codegen.h>
+
+namespace snot {
+namespace x86_64 {
+	class Codegen : public snot::Codegen {
+	private:
+		x86_64::Assembler m_Assembler;
+	public:
+		
+	};
+}
+}
+
+#endif /* end of include guard: CODEGEN_H_1VNJIVK0 */
