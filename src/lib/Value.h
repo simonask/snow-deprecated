20f9a16d995fb475d776857c6fe38f7b0e216e00
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-12-19 13:59:55 +0100

    New type system

--- src/lib/Value.h
+++ src/lib/Value.h
@@ -10,32 +10,33 @@ class Object;
 typedef void* VALUE;
 
 enum ValueType {
-	kObjectType,
-	kUndefinedType,
-	kBooleanType,
-	kIntegerType,
-	kNumValueTypes
+	kObjectType = 0,
+	kEvenIntegerType = 0x1,
+	kOddIntegerType = 0x3,
+	kSpecialType = 0x2,
+	kTypeMask = 0x3
 };
 
 enum SpecialValue {
-	kUndefined = 0x0,
-	kFalse = 0x2,
-	kTrue = 0x4,
+	kTrue = 0x2,
+	kFalse = 0x6,
+	kUndefined = 0xe,
 	kSpecialValueMax
 };
 
-inline bool is_object(VALUE val) { return !((uint64_t)val & 1) && (uint64_t)val > (uint64_t)kSpecialValueMax; }
-inline bool is_special(VALUE val) { return (uint64_t)val & 1 && (uint64_t)val < kSpecialValueMax; }
-inline bool is_undefined(VALUE val) { return (uint64_t)val == kUndefined; }
-inline bool is_boolean(VALUE val) { return (uint64_t)val == kFalse || (uint64_t)val == kTrue; }
-inline bool is_true(VALUE val) { return (uint64_t)val == kTrue; }
-inline bool is_false(VALUE val) { return (uint64_t)val == kFalse; }
-inline bool is_integer(VALUE val) { return (uint64_t)val & 0x01 == 1; }
+inline ValueType value_type(VALUE val) { return (ValueType)((uint64_t)val & kTypeMask); }
+inline SpecialValue special_value(VALUE val) { return (SpecialValue)(uint64_t)val; }
 
-ValueType value_type(VALUE val);
+inline bool is_integer(VALUE val) { return value_type(val) == kEvenIntegerType || value_type(val) == kOddIntegerType; }
+inline bool is_object(VALUE val) { return value_type(val) == kObjectType; }
+inline bool is_special(VALUE val) { return value_type(val) == kSpecialType; }
+inline bool is_true(VALUE val) { return (int64_t)val == kTrue; }
+inline bool is_false(VALUE val) { return (int64_t)val == kFalse; }
+inline bool is_boolean(VALUE val) { return is_true(val) || is_false(val); }
+inline bool is_undefined(VALUE val) { return (int64_t)val == kUndefined; }
 
 inline VALUE value(Object* obj) { return (VALUE)obj; }
-inline VALUE value(int64_t integer) { return (VALUE)((integer << 1) | 1); }
+inline VALUE value(int64_t integer) { return (VALUE)((integer << 1) + 1); }
 inline VALUE value(bool b) { return (VALUE)(b ? kTrue : kFalse); }
 inline VALUE undefined() { return (VALUE)kUndefined; }
 
