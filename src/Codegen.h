4611c3f030d50c251856448209354a992f03793f
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-12-27 15:55:36 +0100

    New scope/local codegen interface

--- src/Codegen.h
+++ src/Codegen.h
@@ -4,7 +4,7 @@
 #include "SymbolTable.h"
 #include "CompiledCode.h"
 #include "Label.h"
-#include "Local.h"
+#include "Scope.h"
 
 #include <string>
 
@@ -15,15 +15,19 @@ public:
 	virtual ~Codegen() {}
 	
 	virtual CompiledCode compile() = 0;
-	virtual Local local(const char* name = 0) = 0;
-	virtual void function_entry(int num_locals) = 0;
+	virtual Scope function_entry(int num_locals) = 0;
 	virtual void function_return() = 0;
-	virtual void set_argument(int index, const Local& src) = 0;
+	virtual void set_argument(int index, const Scope::Local& src) = 0;
+	virtual void set_argument(int index, const Scope::Temporary& src) = 0;
 	virtual void set_argument(int index, const void* ptr) = 0;
 	virtual void set_argument(int index, int immediate) = 0;
-	virtual void get_argument(int index, const Local& dst) = 0;
-	virtual void set_return(const Local& src) = 0;
-	virtual void call(const char* symbol, const Local& retval) = 0;
+	virtual void get_argument(int index, const Scope::Local& dst) = 0;
+	virtual void get_argument(int index, const Scope::Temporary& dst) = 0;
+	virtual void set_return(const Scope::Local& src) = 0;
+	virtual void set_return(const Scope::Temporary& src) = 0;
+	virtual void call(const char* symbol, const Scope::Local& retval) = 0;
+	virtual void call(const char* symbol, const Scope::Temporary& retval) = 0;
+	virtual void debug_break() = 0;
 };
 }
 
---
+++ src/Scope.h
@@ -0,0 +1,37 @@
+#ifndef SCOPE_H_IRS8EJUA
+#define SCOPE_H_IRS8EJUA
+
+#include <vector>
+
+namespace snot {
+	class Scope {
+	public:
+		struct Local {
+			Scope& scope;
+			int index;
+			const char* name;
+			
+			Local(Scope& s, int idx, const char* name = NULL) : scope(s), index(idx), name(name) {}
+			Local(const Local& other) : scope(other.scope), index(other.index) {}
+		};
+		
+		struct Temporary {
+			Scope& scope;
+			int index;
+			Temporary(Scope& s, int idx) : scope(s), index(idx) {}
+			Temporary(const Temporary& other) : scope(other.scope), index(other.index) {}
+		};
+	private:
+		int m_NextLocal;
+		std::vector<Local> m_Locals;
+		int m_NextTemporary;
+		std::vector<Temporary> m_Temporaries;
+	public:
+		Scope() : m_NextLocal(0), m_NextTemporary(0) {}
+		
+		Local local(const char* name = NULL) { return Local(*this, m_NextLocal++, name); }
+		Temporary temp() { return Temporary(*this, m_NextTemporary++); }
+	};
+}
+
+#endif /* end of include guard: SCOPE_H_IRS8EJUA */
