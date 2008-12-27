02080c413db9c7f42ff085c883180e6ef31e96f5
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-12-27 15:59:04 +0100

    Runtime call semantics

--- src/lib/Object.cpp
+++ src/lib/Object.cpp
@@ -11,18 +11,23 @@ namespace snot {
 	}
 	
 	VALUE Object::va_call(VALUE self, uint64_t num_args, va_list& ap) {
+		VALUE call_handler = get("call");
+		if (is_object(call_handler))
+			return object(call_handler)->va_call(self, num_args, ap);
 		return value(this);
 	}
 	
 	VALUE Object::set(const char* member, VALUE value) {
 		m_Members[member] = value;
+		return value;
 	}
 	
 	VALUE Object::get(const char* member) {
 		auto iter = m_Members.find(member);
 		if (iter != m_Members.end())
 			return iter->second;
-		else
-			return undefined();
+		else {
+			return m_Prototype ? m_Prototype->get(member) : undefined();
+		}
 	}
 }
