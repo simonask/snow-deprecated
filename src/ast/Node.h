4dcbfeec0759344154e514a0e389de871806e1a8
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-06 09:27:11 +0100

    a bit more ast node work

--- src/ast/Node.h
+++ src/ast/Node.h
@@ -2,13 +2,53 @@
 #define NODE_H_WML097FE
 
 #include "Basic.h"
+#include "Codegen.h"
+#include <string>
+#include <list>
 
 namespace snot {
 namespace ast {
-	template <int C>
 	class Node {
+	public:
+		virtual ~Node() {}
+		
+		virtual void realize(Codegen&) = 0;
+	};
+	
+	class Literal : public Node { };
+	
+	class LiteralString : public Literal {
 	protected:
-		Node* m_Children[C];
+		std::string m_String;
+	public:
+		LiteralString(const std::string& str) : m_String(str) {}
+	};
+	
+	class LiteralInteger : public Literal {
+	protected:
+		
+	};
+	
+	class Sequence : public Node {
+	protected:
+		std::list<Node*> m_Nodes;
+		
+		void add(Node* node) { m_Nodes.push_back(node); }
+	};
+	
+	class Condition : public Node {
+	protected:
+		Node* m_Expression;
+	};
+	
+	class IfCondition : public Condition {
+	protected:
+		Node* m_IfTrue;
+	};
+	
+	class IfElseCondition : public IfCondition {
+	protected:
+		Node* m_Else;
 	};
 }
 }
