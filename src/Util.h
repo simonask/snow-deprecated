502aff2a2e1702c938ce8740b4bee592ec62bea4
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-07 16:19:25 +0100

    typesafe printf... dunno, might be useful

--- src/Util.h
+++ src/Util.h
@@ -1,6 +1,9 @@
 #ifndef UTIL_H_LBLH2YH8
 #define UTIL_H_LBLH2YH8
 
+#include <ostream>
+#include <stdexcept>
+
 namespace snot {
 	void warn(const char* msg, ...);
 	void error(const char* msg, ...);
@@ -10,6 +13,26 @@ namespace snot {
 	template<typename X, typename Y>
 	void print_mem(X* start, Y* end) {
 		print_mem_raw((void*)start, (void*)end);
+	}
+	
+	inline void stream_printf(std::ostream& o, const char* s) {
+		while (*s) {
+			if (*s == '%' && *++s != '%')
+				throw std::runtime_error("invalid format string: missing arguments");
+			o << *s++;
+		}
+	}
+	
+	template <typename T, typename... A>
+	void stream_printf(std::ostream& o, const char* s, const T& value, const A&... args) {
+		while (*s) {
+			if (*s == '%' && *++s != '%') {
+				o << value;
+				return stream_printf(o, ++s, args...);
+			}
+			o << *s++;
+		}
+		throw std::runtime_error("extra arguments passed to stream_printf");
 	}
 }
 
