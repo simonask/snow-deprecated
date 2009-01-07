9de3b70b6a32dcd700d2d8d132918a15469c8767
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-07 09:49:36 +0100

    ast: use RefPtr, added Literal, Sequence, Condition&friends, Call, Send

--- src/ast/Node.h
+++ src/ast/Node.h
@@ -12,43 +12,87 @@ namespace ast {
 	public:
 		virtual ~Node() {}
 		
-		virtual void realize(Codegen&) = 0;
+		virtual void realize(Codegen&) {};
+		
+		template <class T>
+		bool is_a() const { return dynamic_cast<T*>(this) != NULL; }
 	};
 	
-	class Literal : public Node { };
-	
-	class LiteralString : public Literal {
+	class Literal : public Node {
+	public:
+		enum Type {
+			STRING_TYPE,
+			INTEGER_TYPE,
+			INTEGER_DEC_TYPE = INTEGER_TYPE,
+			INTEGER_HEX_TYPE,
+			INTEGER_BIN_TYPE,
+			FLOAT_TYPE,
+		};
 	protected:
 		std::string m_String;
+		Type m_Type;
 	public:
-		LiteralString(const std::string& str) : m_String(str) {}
+		Literal(const std::string& str, Type type) : m_String(str), m_Type(type) {}
 	};
 	
-	class LiteralInteger : public Literal {
+	class Identifier : public Node {
 	protected:
-		
+		std::string m_Name;
+	public:
+		Identifier(const std::string& name) : m_Name(name) {}
 	};
 	
 	class Sequence : public Node {
 	protected:
-		std::list<Node*> m_Nodes;
-		
-		void add(Node* node) { m_Nodes.push_back(node); }
+		std::list<RefPtr<Node>> m_Nodes;
+	public:
+		void add(RefPtr<Node> node) { m_Nodes.push_back(node); }
+		std::list<RefPtr<Node>>& nodes() { return m_Nodes; }
+		const std::list<RefPtr<Node>>& nodes() const { return m_Nodes; }
+		virtual void realize(Codegen&);
 	};
 	
 	class Condition : public Node {
 	protected:
-		Node* m_Expression;
+		RefPtr<Node> m_Expression;
+	public:
+		Condition(RefPtr<Node> expr) : m_Expression(expr) {}
+		virtual void realize(Codegen&);
 	};
 	
 	class IfCondition : public Condition {
 	protected:
-		Node* m_IfTrue;
+		RefPtr<Node> m_IfTrue;
+	public:
+		IfCondition(RefPtr<Node> expr, RefPtr<Node> if_true) : Condition(expr), m_IfTrue(if_true) {}
+		virtual void realize(Codegen&);
 	};
 	
 	class IfElseCondition : public IfCondition {
 	protected:
-		Node* m_Else;
+		RefPtr<Node> m_IfFalse;
+	public:
+		IfElseCondition(RefPtr<Node> expr, RefPtr<Node> if_true, RefPtr<Node> if_false) : IfCondition(expr, if_true), m_IfFalse(if_false) {}
+		virtual void realize(Codegen&);
+	};
+	
+	class Call : public Node {
+	protected:
+		RefPtr<Node> m_Object;
+		RefPtr<Sequence> m_Arguments;
+	public:
+		Call(RefPtr<Node> obj, RefPtr<Sequence> args) : m_Object(obj), m_Arguments(args) {}
+		virtual void realize(Codegen&);
+	};
+	
+	class Send : public Node {
+	protected:
+		RefPtr<Node> m_Self;
+		RefPtr<Node> m_Method;
+		RefPtr<Sequence> m_Arguments;
+	public:
+		Send(RefPtr<Node> self, RefPtr<Node> method, RefPtr<Sequence> args) : m_Self(self), m_Method(method), m_Arguments(args) {}
+		virtual void realize(Codegen&);
 	};
 }
 }
