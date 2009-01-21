555b2ff19c5f71f521a720589bb7c8881c8467a2
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-21 15:30:59 +0100

    added convenience string builder for snow::String

--- src/lib/SnowString.h
+++ src/lib/SnowString.h
@@ -2,7 +2,9 @@
 #define STRING_H_HLAIM4GC
 
 #include "Object.h"
+#include "Runtime.h"
 #include <string>
+#include <sstream>
 
 namespace snow {
 	Object* string_prototype();
@@ -13,10 +15,32 @@ namespace snow {
 	public:
 		String() : Object(string_prototype()), m_String("") {}
 		String(const std::string& str) : Object(string_prototype()), m_String(str) {}
+		template <typename... A>
+		String(const char* format, A... args) : Object(string_prototype()) {
+			std::stringstream ss;
+			build_string(ss, format, args...);
+			m_String = ss.str();
+		}
 		VALUE copy() const { return value(new String(*this)); }
 		
 		operator const std::string&() const { return m_String; }
 		operator const char*() const { return m_String.c_str(); }
+		
+	private:
+		void build_string(std::stringstream& ss, const char* format) {
+			ss << format;
+		}
+		
+		template <typename... A>
+		void build_string(std::stringstream& ss, const char* format, VALUE val, A... args) {
+			while (*format) {
+				if (*format == '%' && *++format != '%') {
+					ss << value_to_string(val);
+					return build_string(ss, format, args...);
+				}
+				ss << *format++;
+			}
+		}
 	};
 }
 
