2192cb41b3d234f0eab9b4f440df38cae2c625ec
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-21 17:21:23 +0100

    nil.to_string is now the string "nil"

--- src/lib/Nil.cpp
+++ src/lib/Nil.cpp
@@ -8,7 +8,7 @@ namespace snow {
 		if (NilPrototype)
 			return NilPrototype;
 		NilPrototype = new Object;
-		NilPrototype->set("to_string", create_string(""));
+		NilPrototype->set("to_string", create_string("nil"));
 		NilPrototype->set("name", create_string("nil"));
 		return NilPrototype;
 	}
