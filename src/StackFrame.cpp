98abe735f5ef32abde2bd56993399e7215c68ec9
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-21 13:15:00 +0100

    stack frame initialization sets locals to NULL by default

--- src/StackFrame.cpp
+++ src/StackFrame.cpp
@@ -23,6 +23,10 @@ namespace snow {
 			// XXX: Static self
 		}
 		
+		for (uint64_t i = 0; i < frame->num_locals; ++i) {
+			frame->locals[i] = NULL;
+		}
+		
 		pthread_setspecific(current_stack_frame_key, (void*)frame);
 	}
 }
