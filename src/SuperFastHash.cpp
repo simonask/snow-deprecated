d59cf694fbcb8a78b129eded21e415272315dfe5
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-01-22 16:45:20 +0100

    SuperFastHash: moved #defines to .cpp

--- src/SuperFastHash.cpp
+++ src/SuperFastHash.cpp
@@ -1,5 +1,16 @@
 #include "SuperFastHash.h"
 
+#undef get16bits
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define get16bits(d) (*((const uint16_t *) (d)))
+#endif
+
+#if !defined (get16bits)
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+
 namespace snow {
 	uint32_t super_fast_hash(const char* data, int len) {
 		uint32_t hash = len;
