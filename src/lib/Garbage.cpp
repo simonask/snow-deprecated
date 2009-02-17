7f17901a86a0e8c9f69d11f536dab5e399e2b04d
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-02-17 23:03:56 +0100

    * Refactored and restructured GC that supports binary blobs.
    Binary blobs are for when a Handle<> is too slow or too memory-consuming (24 bytes per Handle!!). The tradeoff is that you need to manually mark and unmark contents of blobs.
    
    * Added interfaces to deal with manual marking and unmarking.
    * Objects can override Garbage::gc_(un)mark to do additional marking.

--- src/lib/Garbage.cpp
+++ src/lib/Garbage.cpp
@@ -1,8 +1,11 @@
 #include "Garbage.h"
 #include "Handle.h"
 #include "Object.h"
+#include "StackFrame.h"
+#include "Runtime.h"
 #include <pthread.h>
 #include <unordered_map>
+#include <string.h>
 
 namespace snow {
 	#define ALIGNMENT 0x10
@@ -17,28 +20,43 @@ namespace snow {
 		
 		explicit MemoryHeap(size_t sz) : m_Size(sz), m_Offset(0) {
 			m_Data = new byte[sz];
+			
+			#ifdef DEBUG
+			// poison
+			memset(m_Data, 0xcd, sz);
+			#endif
 		}
 		~MemoryHeap() { delete[] m_Data; }
 		
 		inline size_t available() const { return m_Size - m_Offset; }
 		
-		void* alloc(size_t sz) {
-			size_t required_size = sz + (ALIGNMENT - sz % ALIGNMENT);
+		void* alloc(size_t sz, bool blob) {
+			size_t required_size = sizeof(Garbage::Header) + sz;
+			required_size += (ALIGNMENT - required_size % ALIGNMENT);
 			if (available() < required_size)
 				return NULL;
-			Garbage* object = (Garbage*)&m_Data[m_Offset];
+				
+			Garbage::Header* header = (Garbage::Header*)&m_Data[m_Offset];
+			Garbage* object = (Garbage*)&m_Data[m_Offset + sizeof(Garbage::Header)];
 			m_Offset += required_size;
-			object->m_Size = required_size;
-			object->m_Mark = 1;
+			
+			header->size = required_size;
+			header->flags = Garbage::NO_FLAGS;
+			if (blob)
+				header->flags |= Garbage::BLOB;
+			header->generation = 0;
+			
 			return object;
 		}
 		
 		byte* data() { return m_Data; }
-		inline bool contains(Garbage* ptr) {
+		
+		inline bool contains(const void* ptr) {
 			return (byte*)ptr >= &m_Data[0] && (byte*)ptr < &m_Data[m_Offset];
 		}
 		
 		void compact(PointerMap& moved_pointers) {
+			// TODO: It.
 		}
 		
 		class iterator {
@@ -49,12 +67,13 @@ namespace snow {
 			iterator(MemoryHeap& heap, size_t offset) : m_Heap(heap), m_Offset(offset) {}
 			
 			void next() {
-				m_Offset += object()->m_Size;
+				m_Offset += header()->size;
 			}
 		public:
 			iterator(const iterator& other) : m_Heap(other.m_Heap), m_Offset(other.m_Offset) {}
-			Garbage* object() const { return (Garbage*)&m_Heap.data()[m_Offset];}
-			Garbage* operator*() const { return object(); }
+			Garbage::Header* header() const { return (Garbage::Header*)&m_Heap.data()[m_Offset]; }
+			void* pointer() const { return (void*)&m_Heap.data()[m_Offset + sizeof(Garbage::Header)];}
+			void* operator*() const { return pointer(); }
 			iterator& operator++() {
 				next();
 				return *this;
@@ -70,7 +89,7 @@ namespace snow {
 		
 		iterator begin() { return iterator(*this, 0); }
 		iterator end() { return iterator(*this, m_Offset); }
-		typedef Garbage* value_type;
+		typedef void* value_type;
 	};
 	
 	static pthread_key_t young_key;
@@ -93,8 +112,75 @@ namespace snow {
 		return heap;
 	}
 	
-	void* Garbage::operator new(size_t sz) throw() {
-		return young_heap()->alloc(sz);
+	Garbage::Header* Garbage::header(const void* ptr) {
+		if (young_heap()->contains(ptr)) {
+			ptrdiff_t offset = (ptrdiff_t)ptr - (ptrdiff_t)young_heap()->data();
+			Header* h = (Header*)&young_heap()->data()[offset - sizeof(Header)];
+			return h;
+		}
+		return NULL;
+	}
+	
+	void* Garbage::alloc(size_t sz, bool blob) {
+		return young_heap()->alloc(sz, blob);
+	}
+	
+	void Garbage::_mark(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			h->flags |= MARKED;
+			++h->generation;
+		}
+	}
+	
+	void Garbage::_unmark(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			h->flags &= ~MARKED;
+		}
+	}
+	
+	void Garbage::mark(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			if (h->flags & BLOB)
+				_mark(ptr);
+			else
+				static_cast<Garbage*>(ptr)->mark(ptr);
+		}
+	}
+	
+	void Garbage::unmark(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			if (h->flags & BLOB)
+				_unmark(ptr);
+			else
+				static_cast<Garbage*>(ptr)->unmark(ptr);
+		}
+	}
+	
+	bool Garbage::is_marked(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			return h->flags & MARKED;
+		}
+		return false;
+	}
+	
+	bool Garbage::is_blob(void* ptr) {
+		Header* h = header(ptr);
+		if (h) {
+			return h->flags & BLOB;
+		}
+		return false;
+	}
+	
+	inline void Garbage::mark_frame(StackFrame* frame) {
+		Garbage::mark(frame->scope);
+		for (uint64_t i = 0; i < frame->scope->info().num_locals; ++i) {
+			Garbage::mark(frame->locals[i]);
+		}
 	}
 	
 	void Garbage::collect() {
@@ -102,30 +188,40 @@ namespace snow {
 		
 		// First, reset marks
 		for each (iter, *heap) {
-			Garbage* object = *iter;
-			object->unmark();
+			Garbage::unmark(*iter);
 		}
 		
 		// Then, go through the stack, marking objects
-		Handle* handle = Handle::last();
+		// First, the C++ stack...
+		ValueHandle* handle = ValueHandle::last();
 		while (handle) {
-			if (handle->is_object()) {
-				Garbage* object = handle->cast<Garbage>();
-				if (heap->contains(object))
-					object->mark();
-			}
+			Garbage::mark(handle);
 			handle = handle->previous();
 		}
+		// Then, the Snow stack...
+		StackFrame* frame = get_current_stack_frame();
+		while (frame) {
+			mark_frame(frame);
+			frame = frame->previous;
+		}
+		// Then, the static scopes of Snow...
+		frame = get_current_stack_frame();
+		while (frame) {
+			mark_frame(frame);
+			frame = &frame->scope->parent()->stack_frame();
+		}
+		// Marking done.
 		
 		// Then, compact heap
 		std::unordered_map<void*, void*> moved_pointers;
 		
 		int count = 0;
 		for each (iter, *heap) {
-			Garbage* object = *iter;
-			if (!object->m_Mark) {
-				count++;
-				delete object;
+			if (!Garbage::is_marked(*iter)) {
+				if (!Garbage::is_blob(*iter)) {
+					Garbage* object = static_cast<Garbage*>(*iter);
+					delete object;
+				}
 			}
 		}
 		debug("there were %d unmarked objects", count);
--- src/lib/Garbage.h
+++ src/lib/Garbage.h
@@ -4,22 +4,82 @@
 #include "Basic.h"
 
 namespace snow {
+	struct StackFrame;
+	
 	class Garbage {
 		friend class MemoryHeap;
 	private:
-		size_t m_Size;
-		int m_Mark;
-	protected:
-		void mark() { ++m_Mark; }
-		void unmark() { m_Mark = 0; }
+		enum Flag {
+			NO_FLAGS   = 0,
+			MARKED     = 1,
+			BLOB       = 1 << 1
+		};
+		
+		struct Header {
+			size_t size;
+			uint32_t flags;
+			uint32_t generation;
+		};
+		
+		static void mark_frame(StackFrame*);
+		
+		Header* gc_header() const { return header(this); };
+		static Header* header(const void* ptr);
+		
+		static void _mark(void* ptr);
+		static void _unmark(void* ptr);
 	public:
+		Garbage() {}
 		virtual ~Garbage() {}
 		
-		void* operator new(size_t sz) throw();
-		void operator delete(void*) {};
+		inline bool gc_is_managed() const { return gc_header() != NULL; }
+	
+		virtual void gc_mark();
+		virtual void gc_unmark();
+		virtual bool gc_is_marked();
 		
+		static void* alloc(size_t sz, bool blob = false);
 		static void collect();
+		static void mark(void* ptr);
+		static void unmark(void* ptr);
+		static bool is_marked(void* ptr);
+		static bool is_blob(void* ptr);
+		
+		void* operator new(size_t sz) { return alloc(sz); };
+		void operator delete(void* ptr) { unmark(ptr); };
+		
+		class Lock {
+		private:
+			bool m_Locked;
+		public:
+			Lock() : m_Locked(false) {}
+			
+			inline bool lock() {
+				if (m_Locked)
+					return false;
+				m_Locked = true;
+				return true;
+			}
+			
+			inline void unlock() {
+				m_Locked = false;
+			}
+			
+			inline bool is_locked() const { return m_Locked; }
+		};
 	};
+	
+	inline void Garbage::gc_mark() {
+		Garbage::_mark(this);
+	}
+	
+	inline void Garbage::gc_unmark() {
+		Garbage::_unmark(this);
+	}
+	
+	inline bool Garbage::gc_is_marked() {
+		return Garbage::is_marked(this);
+	}
 }
 
 #endif /* end of include guard: GARBAGE_H_N12SKHS0 */
