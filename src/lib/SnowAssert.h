741ba648ed1491d3fb72f0b222952cbf46e914c4
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-02-17 23:06:01 +0100

    * New assert macros. Need further polishing. * Added TRAP() macro for debug breaks/exceptions.

--- src/lib/SnowAssert.h
+++ src/lib/SnowAssert.h
@@ -3,7 +3,18 @@
 
 #include <assert.h>
 
-#define assert_object(val, T) { assert(object_cast<T>(val) && "expected pointer to object of class " #T); }
-#define assert_args(expr) { assert(expr && "wrong number of arguments"); }
+#ifdef DEBUG
+
+#define ASSERT_OBJECT(val, T) { assert(object_cast<T>(val) && "expected pointer to object of class " #T); }
+#define ASSERT_ARGS(expr) { assert(expr && "wrong number of arguments"); }
+#define TRAP() { __asm__("int3"); }
+
+#else
+
+#define ASSERT_OBJECT()
+#define ASSERT_ARGS()
+#define TRAP()
+
+#endif
 
 #endif /* end of include guard: ASSERT_H_80BKD8NH */
