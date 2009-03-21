982eccd53c2367df11d3157cce18d76d242e49f4
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-03-21 21:29:19 +0100

    preliminary main.cpp

--- main.cpp
+++ main.cpp
@@ -1,275 +1,36 @@
-#include <sys/mman.h>
-#include "x86_64/Operand.h"
-#include "x86_64/Assembler.h"
-#include "x86_64/Disassembler.h"
-#include "x86_64/Codegen.h"
-#include "ASTNode.h"
-#include "CompiledCode.h"
-#include "Linker.h"
-#include "lib/IncrementalAlloc.h"
-#include "Util.h"
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include "lib/Kernel.h"
+#include "parser/Driver.h"
+#include "Codegen.h"
 
-#include "lib/Runtime.h"
-#include "lib/Object.h"
-#include "lib/Array.h"
-#include "lib/SnowString.h"
+#include <vector>
+#include <string>
 
-#define __ masm.
-
-using namespace snow;
-//using namespace snow::x86_64;
-
-void test_codegen() {/*
-	SymbolTable table;
-	table["snow_create_object"] = (void*)snow::create_object;
-	table["snow_send"] = (void*)snow::send;
-	table["snow_call"] = (void*)snow::call;
-	table["snow_destroy"] = (void*)snow::destroy;
-
-	x86_64::Codegen masm;
-
-	__ function_entry(3);
-	auto l1 = Scope::Local(0, "hejsa");
-	__ get_argument(0, l1);
-	auto l2 = Scope::Local(1);
-	__ get_argument(1, l2);
-
-	__ set_argument(0, l1);
-	__ set_argument(1, "+");
-	__ set_argument(2, 1);
-	__ set_argument(3, l2);
-	auto retval = Scope::Local(2);
-	__ call("snow_send", retval);
-
-	__ set_argument(0, l1);
-	__ call("snow_destroy");
-	__ set_argument(0, l2);
-	__ call("snow_destroy");
-
-//	__ debug_break();
-	__ set_return(retval);
-	__ function_return();
-
-
-	RefPtr<CompiledCode> code = __ compile();
-	code->export_symbols(table);
-	code->link(table);
-
-	code->make_executable();
-
-	print_mem(code->code(), &code->code()[code->size()]);
-	printf("code is at 0x%llx\n", (uint64_t)code->code());
-
-	VALUE(*entry)(VALUE a, VALUE b) = (VALUE(*)(VALUE, VALUE))code->code();
-	printf("add: %lld\n", integer(entry(value(-67LL), value(-45LL))));*/
-}
-
-static VALUE global_puts(VALUE self, uint64_t num_args, VALUE* args) {
-	for (uint64_t i = 0; i < num_args; ++i) {
-		printf("%s\n", value_to_string(args[i]));
-	}
-	return nil();
-}
-
-void test_ast() {
-	using namespace snow::ast;
+namespace snow {
+	using namespace std;
 	
-	SymbolTable table;
-	table["snow_eval_truth"] = (void*)snow::eval_truth;
-	table["snow_call"] = (void*)snow::call;
-	table["snow_get"] = (void*)snow::get;
-	table["snow_set"] = (void*)snow::set;
-	table["snow_enter_scope"] = (void*)snow::enter_scope;
-	table["snow_leave_scope"] = (void*)snow::leave_scope;
-	table["snow_get_local"] = (void*)snow::get_local;
+	struct CmdOptions {
+		string cmd;
+		vector<string> filenames;
+	};
 	
-	RefPtr<FunctionDefinition> scope = new FunctionDefinition;
-	scope->arguments.push_back(new Identifier("c"));
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 0", Literal::STRING_TYPE))));
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Identifier("c"))));
-	scope->add(new Assignment(new Identifier("a"), new Literal("123", Literal::INTEGER_TYPE)));
-	scope->add(new Assignment(new Identifier("b"), new Literal("567", Literal::INTEGER_TYPE)));
-	scope->add(new Assignment(
-			new Identifier("d"),
-			new Call(
-				new Identifier("a"),
-				new Identifier("+"),
-				new Sequence(new Identifier("b"))
-			)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 1", Literal::STRING_TYPE))));
-	
-	scope->add(new Assignment(
-			new Identifier("d"),
-			new Call(
-				new Identifier("d"),
-				new Identifier("+"),
-				new Sequence(new Identifier("c"))
-			)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 2", Literal::STRING_TYPE))));
-	
-	scope->add(new Assignment(
-			new Identifier("e"),
-			new Literal("0", Literal::INTEGER_TYPE)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 3", Literal::STRING_TYPE))));
-	
-	scope->add(new Loop(
-			new Call(
-				new Identifier("e"),
-				new Identifier("<"),
-				new Sequence(new Identifier("d"))
-			),
-			new Sequence(
-				new Call(
-					new Identifier("puts"),
-					new Sequence(new Identifier("e"))
-				),
-				new Assignment(new Identifier("e"),
-					new Call(
-						new Identifier("e"),
-						new Identifier("+"),
-						new Sequence(new Literal("1", Literal::INTEGER_TYPE))
-					)
-				)
-			)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 4", Literal::STRING_TYPE))));
-	
-	scope->add(new IfCondition(
-			new Call(
-				new Identifier("e"),
-				new Identifier("="),
-				new Sequence(new Identifier("d"))
-			),
-			new Call(
-				new Identifier("puts"),
-				new Sequence(new Literal("HELLO WORLD!", Literal::STRING_TYPE))
-			)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 5", Literal::STRING_TYPE))));
-	
-	scope->add(new IfElseCondition(
-			new Call(
-				new Identifier("e"),
-				new Identifier("="),
-				new Sequence(new Identifier("c"))
-			),
-			new Call(
-				new Identifier("puts"),
-				new Sequence(new Literal("YES", Literal::STRING_TYPE))
-			),
-			new Call(
-				new Identifier("puts"),
-				new Sequence(new Literal("NO", Literal::STRING_TYPE))
-			)
-		)
-	);
-	
-	scope->add(new Call(new Identifier("puts"), new Sequence(new Literal("TAP 6", Literal::STRING_TYPE))));
-	
-	scope->add(new Return(new Identifier("d")));
-	scope->add(new Call(new Identifier("self"), new Identifier("puts"), new Sequence(new Literal("Should not be reached", Literal::STRING_TYPE))));
-	
-	RefPtr<Codegen> codegen = Codegen::create(*scope);
-	RefPtr<CompiledCode> cc = codegen->compile();
-	
-	cc->export_symbols(table);
-	cc->link(table);
-	cc->make_executable();
-	
-	Handle<Scope> global_scope = new Scope;
-	
-	
-	auto gp = new Function(global_puts);
-	global_scope->set_local("puts", gp);
-	debug("puts is at 0x%llx", gp);
-	
-	printf("code is at: 0x%llx\n", (uint64_t)cc->code());
-	
-	ofstream disasm_file("disasm.log");
-	disasm_file << x86_64::Disassembler::disassemble(*cc, table);
-	disasm_file.close();
-	
-	Handle<Function> f = new Function(*cc);
-	f->set_parent_scope(global_scope);
-	VALUE ret = f->call(nil(), new Array((VALUE[]){value(8LL), value(6LL),value(7LL), value(5LL), value(5LL)}, 5));
-//	printf("returned: %s\n", value_to_string(ret));
-}
-
-void test_sib() {
-	using namespace snow::x86_64;
-	x86_64::Assembler masm;
-	
-	__ enter(32);
-	__ mov(3LL, rax);
-	__ mov(SIB(rdi, rax, SIB::SCALE_8), rbx);
-	__ mov(rbx, rax);
-	__ debug_break();
-	__ leave();
-	__ ret();
-	
-	RefPtr<CompiledCode> cc = masm.compile();
-	cc->make_executable();
-	
-	int64_t* array = new int64_t[10];
-	for (int i = 0; i < 10; ++i) {
-		array[i] = 123 + i;
+	void parse_args(int argc, const char** argv, CmdOptions& opts) {
+		opts.cmd = argv[0];
+		for (int i = 1; i < argc; ++i) {
+			opts.filenames.push_back(argv[i]);
+			// TODO: Real option parsing.
+		}
 	}
 	
-	debug("array is at 0x%llx", array);
-	debug("code is at 0x%llx", cc->function_pointer());
-	Handle<Function> f = new Function(*cc);
-	int64_t ret = (int64_t)f->call(array, new Array);
-	
-	delete[] array;
-	
-	debug("returned: %lld", ret);
+	int main(int argc, const char** argv) {
+		CmdOptions opts;
+		parse_args(argc, argv, opts);
+		
+		for each (iter, opts.filenames) {
+			
+		}
+	}
 }
 
-void print_gc_stats() {
-	const IAllocator::Statistics& s(MemoryManager::statistics(kGarbage));
-
-	printf("=== GC STATS ===\n");
-	
-	printf("allocated_size: %lu\n", s.allocated_size);
-	printf("allocated_objects: %lu\n", s.allocated_objects);
-	printf("collected_size: %lu\n", s.freed_size);
-	printf("collected_objects: %lu\n", s.freed_objects);
-}
-
-int main (int argc, char const *argv[])
-{
-	//Garbage::collect();
-	test_ast();
-	//test_sib();
-	//TempAllocator<ast::Node>::flush();
-	//Garbage::collect();
-	
-	print_gc_stats();
-	
-	debug("sizeof(Object): %lu", sizeof(Object));
-	debug("sizeof(ThinObject): %lu", sizeof(ThinObject));
-	debug("sizeof(Array): %lu", sizeof(Array));
-	debug("sizeof(Scope): %lu", sizeof(Scope));
-	debug("sizeof(Function): %lu", sizeof(Function));
-	debug("sizeof(String): %lu", sizeof(String));
-	debug("sizeof(std::string): %lu", sizeof(std::string));
-	
-	return 0;
+int main (int argc, const char** argv) {
+	return snow::main(argc, argv);
 }
