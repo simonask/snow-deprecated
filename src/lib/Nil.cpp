35f51347a5302511def978b8fe2878564865e669
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-03-22 00:05:49 +0100

    Nil.to_string

--- src/lib/Nil.cpp
+++ src/lib/Nil.cpp
@@ -2,11 +2,15 @@
 #include "Runtime.h"
 
 namespace snow {
+	VALUE nil_to_string(VALUE self, uint64_t num_args, VALUE*) {
+		return create_string("");
+	}
+	
 	Handle<Object>& nil_prototype() {
 		static Handle<Object> proto;
 		if (proto) return proto;
 		proto = new Object;
-		proto->set("to_string", create_string(""));
+		proto->set("to_string", new Function(nil_to_string));
 		proto->set("name", create_string("nil"));
 		return proto;
 	}
