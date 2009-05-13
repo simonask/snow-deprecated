ea165d90bab80055225040f877d473e548ba8a32
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-05-14 00:24:49 +0200

    added TrueType and FalseType for template magic

--- src/base/Basic.h
+++ src/base/Basic.h
@@ -6,14 +6,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#ifndef byte
-#define byte unsigned char
-#endif
+namespace snow {
+	typedef unsigned char byte;
+
+	// Used for template magic
+	struct TrueType {};
+	struct FalseType {};
+}
 
 #define CAPI extern "C"
 
 #include "Iterate.h"
 #include "Util.h"
 #include "RefPtr.h"
+
 
 #endif /* end of include guard: BASIC_H_T0Q2U1RW */
