f13292c6dca63ad73dc82441f61c2ab83e87d7e1
Robin Kaarsgaard <blodmangel@gmail.com>
2009-05-14 16:31:42 +0200

    Numeric interface pretty much feature complete - includes basic mathematical operators and relational operators.

--- src/runtime/Float.cpp
+++ src/runtime/Float.cpp
@@ -6,17 +6,20 @@
 
 namespace snow {	
 	static VALUE float_to_i(VALUE self, uint64_t num_args, VALUE* args) {
+		NORMAL_SCOPE();
 		ASSERT(is_float(self));
 		int64_t a = (int64_t)floatnum(self);
 		return value(a);
 	}
 	
 	static VALUE float_to_f(VALUE self, uint64_t num_args, VALUE* args) {
+		NORMAL_SCOPE();
 		ASSERT(is_float(self));
 		return self;
 	}
 	
 	static VALUE float_to_string(VALUE self, uint64_t num_args, VALUE* args) {
+		NORMAL_SCOPE();
 		ASSERT_ARGS(num_args == 0);
 		std::stringstream ss;
 		ss << floatnum(self);
@@ -31,10 +34,18 @@ namespace snow {
 		proto->set_raw_s("-", new Function(numeric_sub));
 		proto->set_raw_s("*", new Function(numeric_mul));
 		proto->set_raw_s("/", new Function(numeric_div));
-		proto->set_raw_s("to_i", new Function(float_to_i));
+		proto->set_raw_s("%", new Function(numeric_mod));
+		proto->set_raw_s("**", new Function(numeric_power));
+		proto->set_raw_s("<", new Function(numeric_lt));
+		proto->set_raw_s("<=", new Function(numeric_lte));
+		proto->set_raw_s(">", new Function(numeric_gt));
+		proto->set_raw_s(">=", new Function(numeric_gte));
+		
+		VALUE to_string = new Function(float_to_string);
+		proto->set_raw_s("to_string", to_string);
+		proto->set_raw_s("inspect", to_string);
 		proto->set_raw_s("to_f", new Function(float_to_f));
-		proto->set_raw_s("to_string", new Function(float_to_string));
-		proto->set_raw_s("inspect", new Function(float_to_string));
+		proto->set_raw_s("to_i", new Function(float_to_i));
 		return proto;
 	}
 }
--- src/runtime/Integer.cpp
+++ src/runtime/Integer.cpp
@@ -5,32 +5,10 @@
 #include "Function.h"
 #include "Exception.h"
 #include <sstream>
-#include <cmath>
 
 #define FUNC new(kMalloc) Function //;
 
 namespace snow {
-	static VALUE integer_mod(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		if (num_args == 0)
-			return self;
-		int64_t a = integer(self);
-		ASSERT(is_integer(args[0]));
-		int64_t b = integer(args[0]);
-		return value(a % b);
-	}
-
-	static VALUE integer_power(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		if (num_args == 0)
-			return self;
-		int64_t a = integer(self);
-		ASSERT(is_integer(args[0]));
-		int64_t b = integer(args[0]);
-		int64_t v = pow(a,b);
-		return value(v);
-	}
-
 	static VALUE integer_to_string(VALUE self, uint64_t num_args, VALUE* args) {
 		NORMAL_SCOPE();
 		std::stringstream ss;
@@ -60,57 +38,32 @@ namespace snow {
 		return value((float)integer(self));
 	}
 	
-	static VALUE integer_lt(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		ASSERT_ARGS(num_args >= 1);
-		return value(integer(self) < integer(args[0]));
-	}
-	
-	static VALUE integer_lte(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		ASSERT_ARGS(num_args >= 1);
-		return value(integer(self) <= integer(args[0]));
-	}
-	
-	static VALUE integer_gt(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		ASSERT_ARGS(num_args >= 1);
-		return value(integer(self) > integer(args[0]));
-	}
-	
-	static VALUE integer_gte(VALUE self, uint64_t num_args, VALUE* args) {
-		NORMAL_SCOPE();
-		ASSERT_ARGS(num_args >= 1);
-		return value(integer(self) >= integer(args[0]));
-	}
-	
-	static VALUE integer_sqrt(VALUE self, uint64_t num_args, VALUE* args) {
+	static VALUE integer_to_i(VALUE self, uint64_t num_args, VALUE* args) {
 		NORMAL_SCOPE();
 		ASSERT_ARGS(num_args == 0);
-		float v = sqrt(integer(self));
-		return value(v);
+		return self;
 	}
 	
- 	Object* integer_prototype() {
+	Object* integer_prototype() {
 		static Object* ip = NULL;
 		if (ip) return ip;
 		ip = new(kMalloc) Object;
-		ip->set_raw_s("name", new(kMalloc) String("Integer"));
 		ip->set_raw_s("+", FUNC(numeric_add));
 		ip->set_raw_s("-", FUNC(numeric_sub));
 		ip->set_raw_s("*", FUNC(numeric_mul));
 		ip->set_raw_s("/", FUNC(numeric_div));
-		ip->set_raw_s("%", FUNC(integer_mod));
-		ip->set_raw_s("**", FUNC(integer_power));
+		ip->set_raw_s("%", FUNC(numeric_mod));
+		ip->set_raw_s("**", FUNC(numeric_power));
+		ip->set_raw_s("<", FUNC(numeric_lt));
+		ip->set_raw_s("<=", FUNC(numeric_lte));
+		ip->set_raw_s(">", FUNC(numeric_gt));
+		ip->set_raw_s(">=", FUNC(numeric_gte));
+		
 		VALUE to_string = FUNC(integer_to_string);
 		ip->set_raw_s("to_string", to_string);
 		ip->set_raw_s("inspect", to_string);
 		ip->set_raw_s("to_f", FUNC(integer_to_f));
-		ip->set_raw_s("<", FUNC(integer_lt));
-		ip->set_raw_s("<=", FUNC(integer_lte));
-		ip->set_raw_s(">", FUNC(integer_gt));
-		ip->set_raw_s(">=", FUNC(integer_gte));
-		ip->set_raw_s("sqrt", FUNC(integer_sqrt));
+		ip->set_raw_s("to_i", FUNC(integer_to_i));
 		return ip;
 	}
 }
